e198e5bf348d10657edb3d39ef4d9d2cd72debc1
jmpark24 <jmpark@gmail.com>
2024-01-09 18:35:29 +0900

    반응형 레이아웃 작업

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function hideBasket() {
 }
 
 
-// 검색1
+// 검색!
 const headerEl = document.querySelector('header');
 const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')];
 const searchWrapEl = headerEl.querySelector('.search-wrap');
@@ -40,12 +40,15 @@ const searchInputEl = searchWrapEl.querySelector('input');
 const searchDelayEl = [...searchWrapEl.querySelectorAll('li')];
 
 searchStarterEl.addEventListener('click', showSearch);
-searchCloserEl.addEventListener('click', hideSearch);
+searchCloserEl.addEventListener('click', function (event) {
+  event.stopPropagation();
+  hideSearch();
+});
 searchShadowEl.addEventListener('click', hideSearch);
 
 function showSearch() {
   headerEl.classList.add('searching');
-  document.documentElement.classList.add('fixed');
+  stopScroll();
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
   });
@@ -58,7 +61,7 @@ function showSearch() {
 }
 function hideSearch() {
   headerEl.classList.remove('searching');
-  document.documentElement.classList.remove('fixed');
+  playScroll();
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
   });
@@ -66,7 +69,70 @@ function hideSearch() {
     el.style.transitionDelay = index * .4 / searchDelayEl.length + 's';
   });
   searchDelayEl.reverse();
-  searchInputEl.value = "";
+  searchInputEl.value = '';
+}
+function playScroll() {
+  document.documentElement.classList.remove('fixed');
+}
+function stopScroll() {
+  document.documentElement.classList.add('fixed');
+}
+
+// 헤더 메뉴 토글!
+const menuStarterEl = document.querySelector('header .menu-starter');
+menuStarterEl.addEventListener('click', function () {
+  if (headerEl.classList.contains('menuing')) {
+    headerEl.classList.remove('menuing');
+    searchInputEl.value = '';
+    playScroll();
+  } else {
+    headerEl.classList.add('menuing');
+    stopScroll();
+  }
+});
+
+// 헤더 겅색!
+const searchTextFieldEl = document.querySelector('header .textfield');
+const searchCancelEl = document.querySelector('header .search-canceler');
+searchTextFieldEl.addEventListener('click', function () {
+  headerEl.classList.add('searching--mobile');
+  searchInputEl.focus();
+});
+searchCancelEl.addEventListener('click', function () {
+  headerEl.classList.remove('searching--mobile');
+});
+
+// 
+window.addEventListener('resize', function () {
+  if (window.innerWidth <= 740) {
+    headerEl.classList.remove('searching');
+  } else {
+    headerEl.classList.remove('searching--mobile');
+  }
+});
+
+//
+const navEl = document.querySelector('nav');
+const navMenuToggleEl = navEl.querySelector('.menu-toggler');
+const navMenuShadowEl = navEl.querySelector('.shadow');
+
+navMenuToggleEl.addEventListener('click', function () {
+  if (navEl.classList.contains('menuing')) {
+    hideNavMenu();
+  } else {
+    showNavMenu();
+  }
+});
+navEl.addEventListener('click', function (event) {
+  event.stopPropagation();
+});
+navMenuShadowEl.addEventListener('click', hideNavMenu);
+window.addEventListener('click', hideNavMenu);
+function showNavMenu() {
+  navEl.classList.add('menuing');
+}
+function hideNavMenu() {
+  navEl.classList.remove('menuing');
 }
 
 // 요소의 가시성 관찰
@@ -145,6 +211,7 @@ navigations.forEach(function (nav) {
   mapEl.innerHTML = /*html*/`
     <h3>
       <span class="text">${nav.title}</span>
+      <span class="icon">+</span>
     </h3>
     <ul>
       ${mapList}
@@ -155,3 +222,12 @@ navigations.forEach(function (nav) {
 
 const thisYearEl = document.querySelector('span.this-year');
 thisYearEl.textContent = new Date().getFullYear();
+
+
+const mapEls = document.querySelectorAll('footer .navigations .map');
+mapEls.forEach(function (el) {
+  const h3El = el.querySelector('h3');
+  h3El.addEventListener('click', function () {
+    el.classList.toggle('active');
+  });
+});
